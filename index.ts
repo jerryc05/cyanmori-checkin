b697e4671ef24077948b8cb8f9cec1b203e2fd3f
jerryc05 <jerryc443@gmail.com>
2024-01-20 18:14:30 -0500

    u

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log(new Date())
+console.dir(process.env)
+// fetch("https://cccc.gg/user/checkin", {
+//   "headers": {
+//     "accept": "application/json, text/javascript, */*; q=0.01",
+//     "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+//     "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-ch-ua-platform": "\"macOS\"",
+//     "sec-fetch-dest": "empty",
+//     "sec-fetch-mode": "cors",
+//     "sec-fetch-site": "same-origin",
+//     "x-requested-with": "XMLHttpRequest",
+//     "cookie": process.env.COOKIE,
+//     "Referer": "https://cccc.gg/user",
+//     "Referrer-Policy": "strict-origin-when-cross-origin"
+//   },
+//   "body": null,
+//   "method": "POST"
+// }).then(x=>x.json()).then(console.log).finally(console.error)
