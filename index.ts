5255307ddd090f59e627e52a5a1436098a3a29ef
jerryc05 <jerryc443@gmail.com>
2024-01-20 19:59:16 -0500

    u

--- index.ts
+++ index.ts
@@ -51,34 +51,42 @@ function mergeCookies(existingCookies: string, setCookieHeaders: string[]) {
     .join('; ')
 }
 
-fetch('https://cccc.gg/user/checkin', {
-  headers: {
-    accept: 'application/json, text/javascript, */*; q=0.01',
-    'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
-    'sec-ch-ua':
-      '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'x-requested-with': 'XMLHttpRequest',
-    cookie: process.env.COOKIE,
-    Referer: 'https://cccc.gg/user',
-    'Referrer-Policy': 'strict-origin-when-cross-origin',
-  },
-  body: null,
-  method: 'POST',
-})
-  .then(x => {
-    if (x.headers.get('Set-Cookie'))
-      updateSecret(
-        'jerryc05',
-        'cyanmori-checkin',
-        'COOKIE',
-        mergeCookies(process.env.COOKIE, x.headers.getSetCookie())
-      ).catch(console.error)
-    return x.json()
+const promises = []
+
+promises.push(
+  fetch('https://cccc.gg/user/checkin', {
+    headers: {
+      accept: 'application/json, text/javascript, */*; q=0.01',
+      'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+      'sec-ch-ua':
+        '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'x-requested-with': 'XMLHttpRequest',
+      cookie: process.env.COOKIE,
+      Referer: 'https://cccc.gg/user',
+      'Referrer-Policy': 'strict-origin-when-cross-origin',
+    },
+    body: null,
+    method: 'POST',
   })
-  .then(console.log)
-  .finally(console.error)
+    .then(x => {
+      if (x.headers.get('Set-Cookie'))
+        promises.push(
+          updateSecret(
+            'jerryc05',
+            'cyanmori-checkin',
+            'COOKIE',
+            mergeCookies(process.env.COOKIE, x.headers.getSetCookie())
+          ).catch(console.error)
+        )
+      return x.json()
+    })
+    .then(console.log)
+    .finally(console.error)
+)
+
+await Promise.allSettled(promises)
